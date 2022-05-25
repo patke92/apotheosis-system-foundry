bea457b9cde6a26062023df25f9c1b38d5b2ef36
Patrick Keßler <dev@patke.info>
2022-05-26 00:01:14 +0200

    adjustments

--- module/helpers/config.mjs
+++ module/helpers/config.mjs
@@ -1,33 +1,5 @@
 export const APOTHEOSIS = {}
 
-// Define constants here, such as:
-APOTHEOSIS.foobar = {
-    bas: "APOTHEOSIS.bas",
-    bar: "APOTHEOSIS.bar",
-}
-
-/**
- * The set of Attribute Scores used within the sytem.
- * @type {Object}
- */
-APOTHEOSIS.attributes = {
-    str: "APOTHEOSIS.AttributeStr",
-    dex: "APOTHEOSIS.AttributeDex",
-    con: "APOTHEOSIS.AttributeCon",
-    int: "APOTHEOSIS.AttributeInt",
-    cha: "APOTHEOSIS.AttributeCha",
-    luck: "APOTHEOSIS.AttributeLuck",
-}
-
-APOTHEOSIS.attributeAbbreviations = {
-    str: "APOTHEOSIS.AttributeStrAbbr",
-    dex: "APOTHEOSIS.AttributeDexAbbr",
-    con: "APOTHEOSIS.AttributeConAbbr",
-    int: "APOTHEOSIS.AttributeIntAbbr",
-    cha: "APOTHEOSIS.AttributeChaAbbr",
-    luck: "APOTHEOSIS.AttributeLuckAbbr",
-}
-
 APOTHEOSIS.checks = {
     athletics: "Athletics",
     grappling: "Grappling",
@@ -51,4 +23,26 @@ APOTHEOSIS.checks = {
     intimidation: "Intimidation",
     persuasion: "Persuasion",
     sleightOfHand: "Sleight of Hand",
+}
+
+/**
+ * The set of Attribute Scores used within the sytem.
+ * @type {Object}
+ */
+APOTHEOSIS.attributes = {
+    str: "APOTHEOSIS.AttributeStr",
+    dex: "APOTHEOSIS.AttributeDex",
+    con: "APOTHEOSIS.AttributeCon",
+    int: "APOTHEOSIS.AttributeInt",
+    cha: "APOTHEOSIS.AttributeCha",
+    luck: "APOTHEOSIS.AttributeLuck",
+}
+
+APOTHEOSIS.attributeAbbreviations = {
+    str: "APOTHEOSIS.AttributeStrAbbr",
+    dex: "APOTHEOSIS.AttributeDexAbbr",
+    con: "APOTHEOSIS.AttributeConAbbr",
+    int: "APOTHEOSIS.AttributeIntAbbr",
+    cha: "APOTHEOSIS.AttributeChaAbbr",
+    luck: "APOTHEOSIS.AttributeLuckAbbr",
 }
